b2b7a29368dde12752467a27aeacffc8dbddd161
olback <git@olback.net>
2018-04-09 12:40:32 +0200

    table styles

---
+++ sample-config.php
@@ -0,0 +1,10 @@
+<?php
+
+    $GLOBALS['username'] = 'admin'; // Default username is 'admin'. Please change!
+    $GLOBALS['password'] = 'x279e'; // Default password is 'x279e. Please change!
+
+    // Path to db file - Don't change unless you know what you're doing!
+    $GLOBALS['dbFile'] = './blogger.db';
+
+    $GLOBALS['git-url'] = 'https://github.com/olback/sqlite-blogger.git';
+
